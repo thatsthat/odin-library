28536c472c808b2c996a570bdd39227f0a42ea20
Aram Anto <aram.anto@gmail.com>
2022-05-11 12:11:19 +0200

    started constructor code

---
+++ index.js
@@ -0,0 +1,9 @@
+function Book(title, author, pages, iveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.iveRead = iveRead;
+  this.info = function () {
+    return `${this.title} by ${this.title}, ${pages}, ${this.iveRead}`;
+  };
+}
