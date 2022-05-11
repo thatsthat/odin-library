d239c6ce9bd2ecc27c376c88120d5626ba8999b1
Aram Anto <aram.anto@gmail.com>
2022-05-11 14:54:12 +0200

    display current books info on viewport

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let myLibrary = [];
 
+const book1 = new Book("Lord of the Rings", "J.R.R. Tolkien", "425", true);
+myLibrary.push(book1);
+const book2 = new Book("The Martian", "Andy Weiss", "353", false);
+myLibrary.push(book2);
+
+// Variable that stores the displayed books
+const shelve = document.querySelector("#container");
+
+myLibrary.forEach((book) => {
+  const bookInfo = document.createElement("div");
+  bookInfo.textContent = book.info();
+  shelve.appendChild(bookInfo);
+});
+
+// Show details of each book in myLibrary
+
 function Book(title, author, pages, iveRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.iveRead = iveRead;
   this.info = function () {
-    return `${this.title} by ${this.title}, ${pages}, ${this.iveRead}`;
+    return `${this.title} by ${this.title}, ${pages}, Read:${this.iveRead}`;
   };
 }
 
