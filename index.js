f7dd3686adeafe2ab86c26b6c8dbefbafbd8cb0d
Aram Anto <aram.anto@gmail.com>
2022-05-12 15:40:08 +0200

    remove book button working!

--- index.js
+++ index.js
@@ -11,27 +11,6 @@ const shelve = document.querySelector("#books");
 // Show details of each book in myLibrary
 myLibrary.forEach((book) => addBookInfo(book));
 
-function addBookInfo(book) {
-  const bookInfo = document.createElement("div");
-  bookInfo.classList.add("book");
-  bookInfo.textContent = book.info();
-  // Create container for buttons
-  const bookButtons = document.createElement("div");
-  bookButtons.classList.add("bookButtons");
-  // Create button allowing user to delete book from shelve
-  const delBook = document.createElement("button");
-  delBook.classList.add("delBook");
-  delBook.textContent = "Remove";
-  bookButtons.appendChild(delBook);
-  // Create button allowing user to toggle read/unread book status
-  const bookRead = document.createElement("button");
-  bookRead.classList.add("bookRead");
-  bookRead.textContent = "Unread";
-  bookButtons.appendChild(bookRead);
-  bookInfo.appendChild(bookButtons);
-  shelve.appendChild(bookInfo);
-}
-
 document.forms["new-book"].addEventListener("submit", (event) => {
   event.preventDefault();
   // TODO do something here to show user that form is being submitted
@@ -55,6 +34,47 @@ document.forms["new-book"].addEventListener("submit", (event) => {
       // TODO handle error
     });
 });
+
+// Add an event listener to each button
+const allButtons = document.querySelectorAll(".delBook, .bookRead");
+allButtons.forEach((button) =>
+  button.addEventListener("click", (event) => {
+    handleButtons(event.target);
+  })
+);
+
+function handleButtons(button) {
+  if (button.textContent === "Remove") {
+    button.parentElement.parentElement.remove();
+  }
+}
+
+function addBookInfo(book) {
+  const bookInfo = document.createElement("div");
+  bookInfo.classList.add("book");
+  bookInfo.textContent = book.info();
+  // Create container for buttons
+  const bookButtons = document.createElement("div");
+  bookButtons.classList.add("bookButtons");
+  // Create button allowing user to delete book from shelve
+  const delBook = document.createElement("button");
+  delBook.classList.add("delBook");
+  delBook.textContent = "Remove";
+  delBook.addEventListener("click", (event) => {
+    handleButtons(event.target);
+  });
+  bookButtons.appendChild(delBook);
+  // Create button allowing user to toggle read/unread book status
+  const bookRead = document.createElement("button");
+  bookRead.classList.add("bookRead");
+  bookRead.textContent = "Unread";
+  bookRead.addEventListener("click", (event) => {
+    handleButtons(event.target);
+  });
+  bookButtons.appendChild(bookRead);
+  bookInfo.appendChild(bookButtons);
+  shelve.appendChild(bookInfo);
+}
 
 function Book(title, author, pages, iveRead) {
   this.title = title;
